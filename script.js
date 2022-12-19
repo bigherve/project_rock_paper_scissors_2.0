5ad2a9fc5ba34026f44b341591d56718ef94bd04
bigherve <hervekunda@gmail.com>
2022-12-19 18:04:14 +0100

    Done writing the playRound function

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function getPcChoice() {
     let random = options[Math.floor(Math.random() * options.length)];
     return random;
 }
+
+function playRound(playerSelection, pcSelection) {
+    playerSelection = getUserChoice();
+    pcSelection = getPcChoice();
+    if (playerSelection === 'rock' && pcSelection === 'paper') {
+        alert('You lose!');
+    } else if (playerSelection === 'rock' && pcSelection === 'scissors') {
+        alert('You win!');
+    } else if (playerSelection === 'paper' && pcSelection === 'rock') {
+        alert('You win!');
+    } else if (playerSelection === 'paper' && pcSelection === 'scissors') {
+        alert('You lose!');
+    } else if (playerSelection === 'scissors' && pcSelection === 'rock') {
+        alert('You lose!');
+    } else if (playerSelection === 'scissors' && pcSelection === 'paper') {
+        alert('You win!');
+    } else if (playerSelection === 'rock' && pcSelection === 'rock' ||
+        playerSelection === 'paper' && pcSelection === 'paper' ||
+        playerSelection === 'scissors' && pcSelection === 'scissors') {
+        alert('It\'s a draw');
+    }
+}
